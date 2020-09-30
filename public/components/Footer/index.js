ec0c8355239f44802b4deabb7f1ae1629a7c84fe
Una Kravets <unakravets@google.com>
2020-09-30 13:47:55 -0400

    more footer styling

--- public/components/Footer/index.js
+++ public/components/Footer/index.js
@@ -1,22 +1,26 @@
 import {Component} from 'preact'
 import style from './style.module.css'
 import navStyle from '../Nav/style.module.css'
+import pageStyle from '../../pages/style.module.css'
 
 export default class Nav extends Component {
   render(props) {
     return (
     <div class={style.footer}>
-      <div>
-        <h1 class={navStyle.logo}>Houdini.how</h1>
+      <div class={pageStyle.container}>
+        <div>
+          <h1 class={navStyle.logo}>Houdini.how</h1>
+          <p>Brought to you by <a href="https://web.dev">web.dev</a></p>
+        </div>
+        <nav>
+          <ul>
+            <li><a href="/about">About</a></li>
+            <li><a href="/usage">Usage</a></li>
+            <li><a href="/resources">Resources</a></li>
+            <li><a href="/">Worklets</a></li>
+          </ul>
+        </nav>
       </div>
-      <nav>
-        <ul>
-          <li><a href="/about">About</a></li>
-          <li><a href="/usage">Usage</a></li>
-          <li><a href="/resources">Resources</a></li>
-          <li><a href="/">Worklets</a></li>
-        </ul>
-      </nav>
     </div>
    )
   }
