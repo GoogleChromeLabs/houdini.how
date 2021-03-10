2dc467ee1e80cb465b4df322d499dd9910f86eff
Kartik <kartikprajapati987@gmail.com>
2021-03-11 02:03:07 +0530

    Restructure Tag component

--- public/components/Tag/index.js
+++ public/components/Tag/index.js
@@ -1,39 +1,43 @@
-import {Component} from 'preact'
-import style from './style.module.css'
+import {Component} from "preact";
+import style from "./style.module.css";
+
+const APIS = [
+  { label: "paint", style: "paint", text: "Paint" },
+  {
+    label: "properties and values",
+    style: "props",
+    text: "Properties & Values",
+  },
+  { label: "typed object model", style: "typed", text: "Typed Object Model" },
+  { label: "layout", style: "layout", text: "Layout" },
+  { label: "animation", style: "animation", text: "Animation" },
+];
 
 export default class Tag extends Component {
   render(props) {
-    if (props.label === 'paint') {
-      return (
-        <li class={`${style.tag} ${props.selected === false ? style.paint : style['paint-active']}`}>
-          <a href="#" onClick={() => {props.setFilter('paint')}}>Paint</a>
-        </li>
-      )
-    } else if (props.label === 'properties and values') {
-      return (
-        <li class={`${style.tag} ${props.selected === false ? style.props : style['props-active']}`}>
-          <a href="#" onClick={() => {props.setFilter('properties and values')}}>Properties & Values</a>
-        </li>
-      )
-    } else if (props.label === 'typed object model') {
-      return (
-        <li class={`${style.tag} ${props.selected === false ? style.typed : style['typed-active']}`}>
-          <a href="#" onClick={() => {props.setFilter('typed object model')}}>Typed Object Model</a>
-        </li>
-      )
-    } else if (props.label === 'layout') {
-      return (
-        <li class={`${style.tag} ${props.selected === false ? style.layout : style['layout-active']}`}>
-          <a href="#" onClick={() => {props.setFilter('layout')}}>Layout</a>
-        </li>
-      )
-    } else if (props.label === 'animation') {
-      return (
-        <li class={`${style.tag} ${props.selected === false ? style.animation : style['animation-active']}`}>
-          <a href="#" onClick={() => {props.setFilter('animation')}}>Animation</a>
-        </li>
-      )
-    }
-    else return (<li></li>);
+    const TAG = APIS.map((api) =>
+      api.label === props.label ? api : null
+    ).filter((api) => api != undefined)[0];
+
+    return (
+      <li
+        class={`${style.tag} ${
+          props.selected === false
+            ? style[TAG.style]
+            : style[`${TAG.style}-active`]
+        }`}
+      >
+        <a
+          href="javascript:void(0)"
+          onClick={(e) => {
+            e.preventDefault();
+            props.setFilter(TAG.label);
+          }}
+          title={TAG.text}
+        >
+          {TAG.text}
+        </a>
+      </li>
+    );
   }
 }
