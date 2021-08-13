e54b5a4d0b5a50ef8353aecef7a6325396f60004
Danish Kamal <danish5095@gmail.com>
2021-08-13 18:15:51 +0530

    Direct Linking to Worklets on Homepage

--- public/components/Card/index.js
+++ public/components/Card/index.js
@@ -14,13 +14,29 @@
 import style from './style.module.css'
 import CardMeta from '../CardMeta/index.js'
 
-export default function Card(props) {
-  return (
-    <div class={style.container} type={props.type} >
-      <CardMeta authorImg={props.authorImg} authorName={props.authorName} authorLink={props.authorLink} packageName={props.name} type={props.type} tags={props.tags} date={props.date} note={props.note}/>
-      <div class={style.card}>
-        {props.children}
+export default class Card {
+  onRefCreated(target) {
+    const {name} = this.props;
+    const {hash} = window.location;
+    requestAnimationFrame(() => {
+      if (target && hash.includes(name)) {
+        target.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start',
+          inline: 'center',
+        });
+      }
+    });
+  }
+
+  render() {
+    return (
+      <div class={style.container} type={this.props.type} id={this.props.packageName} ref={this.onRefCreated.bind(this)}>
+        <CardMeta authorImg={this.props.authorImg} authorName={this.props.authorName} authorLink={this.props.authorLink} packageName={this.props.name} type={this.props.type} tags={this.props.tags} date={this.props.date} note={this.props.note}/>
+        <div class={style.card}>
+          {this.props.children}
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
